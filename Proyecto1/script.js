0a92c8eea3109893a38125fcf5c0b25c57036b8b
JuanRodriguez1219 <110642261+JuanRodriguez1219@users.noreply.github.com>
2023-10-17 19:10:17 -0500

    Update script.js

--- Proyecto1/script.js
+++ Proyecto1/script.js
@@ -5,35 +5,37 @@ botones.forEach(boton => {
     boton.addEventListener("click", () => {
     const botonOn = boton.textContent;
     
-    if(boton.id === "limpiar") {
+if(boton.id === "limpiar") {
         pantalla.textContent = "0";
         return;        
     }
 
-     if(boton.id === "borrar") {
-        if(pantalla.textContent.length ===1 || pantalla.textContent === "¡Error!") {
-            pantalla.textContent = "0";
-        }else{
-        pantalla.textContent = pantalla.textContent.slice(0, -1);
-        }
-        return;        
-    }
-
-    if (boton.id ==="igual") {
+    if(boton.id === "igual"){
         try{
-            pantalla.textContent = eval(pantalla.textContent);
-        } catch {
-            pantalla.textContent = "¡Error!";
+            if(pantalla.textContent.includes("//","**") || pantalla.textContent === "Error"){
+            pantalla.textContent = "error"
+
+
+
+            }else{ pantalla.textContent = eval (pantalla.textContent);}
+        pantalla.textContent = eval(pantalla.textContent);
+        }
+        catch{
+            pantalla.textContent = "error"
         }
         return;
     }
-
-    if (pantalla.textContent === "0") {
-        pantalla.textContent = botonOn;
-    } else{
-    pantalla.textContent += botonOn;
+    const botoEn=boton.textContent;
+    if(pantalla.textContent === "0" || pantalla.textContent === "syntax error"){
+        pantalla.textContent = botoEn;
+        
+    } else {
+        pantalla.textContent += botoEn;
+        return;
     }
+  
 
-    
-    })
+
+})
+
 })
